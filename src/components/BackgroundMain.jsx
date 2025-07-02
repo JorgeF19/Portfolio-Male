672ec89d8341e29b69f50f93bd76c0ce2f8869bb
Jorge Ferrer <jwjf19@gmail.com>
2025-07-02 13:35:07 -0500

    path fix

--- src/components/BackgroundMain.jsx
+++ src/components/BackgroundMain.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const BackgroundMain = styled.img`
+const Background = styled.img`
   position: absolute;
   height: 100%;
   min-width: 100dvw;
@@ -9,7 +9,7 @@ const BackgroundMain = styled.img`
 `;
 
 function BackgroundMainComponent({ src, alt, id }) {
-  return <BackgroundMain id={id} src={src} alt={alt} />;
+  return <Background id={id} src={src} alt={alt} />;
 }
 
 export default BackgroundMainComponent;
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -37,7 +37,7 @@ function Gallery() {
               xmlns="http://www.w3.org/2000/svg"
               stroke="#ffffff"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
                 strokeLinecap="round"
